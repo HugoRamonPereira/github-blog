4938368d3ed259440a810aad593cdab4f76187e4
Hugo Ramon Pereira <hramonp-dev@outlook.com>
2023-12-04 22:51:00 -0300

    finished post page

--- src/views/post/index.tsx
+++ src/views/post/index.tsx
@@ -1,9 +1,38 @@
+import { useCallback, useEffect, useState } from 'react';
 import { PostHeader } from './components/post-header';
+import { BlogPostProps } from '../blog';
+import { api } from '../../lib/axios';
+import { useParams } from 'react-router';
+import { PostContent } from './components/post-content';
+
+const username = import.meta.env.VITE_GITHUB_USERNAME;
+const reponame = import.meta.env.VITE_GITHUB_REPONAME;
 
 export function Post() {
+  const [postInfo, setPostInfo] = useState<BlogPostProps>({} as BlogPostProps);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  const getPostInfo = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await api.get(
+        `/repos/${username}/${reponame}/issues/${id}`
+      );
+      setPostInfo(response.data);
+    } finally {
+      setLoading(false);
+    }
+  }, [postInfo]);
+
+  useEffect(() => {
+    getPostInfo();
+  }, []);
+
   return (
     <>
-      <PostHeader />
+      <PostHeader postInfo={postInfo} loading={loading} />
+      {!loading && <PostContent content={postInfo.body} />}
     </>
   );
 }
